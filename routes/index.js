07eaf47693f67863f842f2ad03689f6382f8c5a5
hgcummings <harry@xlr3.com>
2016-05-02 18:05:09 +0100

    Chapter 02: Using Express middleware

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express', name: 'World' });
+  var visits = parseInt(req.cookies.visits) || 0;
+  visits += 1;
+  res.cookie('visits', visits);
+  res.render('index',
+      { title: 'Express', name: 'World', visits: visits }
+  );
 });
 
 module.exports = router;
