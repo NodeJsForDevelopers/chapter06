30e5c794fc7027af1c134b072f0da2215f509277
hgcummings <harry@xlr3.com>
2016-05-07 12:00:53 +0100

    Chapter 05: Issuing a DELETE request from the client

---
+++ public/scripts/index.js
@@ -0,0 +1,13 @@
+$(function() {
+    'use strict';
+    
+    $('#createdGames .delete').click(function() {
+        var $this = $(this);
+        $.ajax($this.attr('href'), {
+            method: 'delete'
+        }).done(function() {
+            $this.closest('.game').remove();
+        });
+        event.preventDefault();
+    });
+});
