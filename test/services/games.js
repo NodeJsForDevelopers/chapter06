d033d84df10febe0f2d345a6033a55d9e88f55e8
hgcummings <harry@xlr3.com>
2016-05-07 16:20:35 +0100

    Chapter 06: Resetting state between tests

--- test/services/games.js
+++ test/services/games.js
@@ -6,7 +6,12 @@ const service = require('../../src/services/games.js');
 describe('Game service', () => {
     const firstUserId = 'user-id-1';
     const secondUserId = 'user-id-2';
-        
+    
+    beforeEach(() => {
+        let gamesCreated = service.availableTo("not-a-user");
+        gamesCreated.forEach(game => game.remove());
+    });
+    
     describe('list of available games', () => { 
         it('should include games set by other users', () => {
             // Given
