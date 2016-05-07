7608549a046460df9c1b8c7cf4a1cfd124305c78
hgcummings <harry@xlr3.com>
2016-05-07 17:31:12 +0100

    Chapter 06: Simplifying tests using SsuperaAgent

--- test/routes/games.js
+++ test/routes/games.js
@@ -1,60 +1,37 @@
 'use strict';
 
-const http = require('http');
+const request = require('supertest');
 const expect = require('chai').expect;
 
 const app = require('../../src/app.js');
 const gamesService = require('../../src/services/games.js');
 
-const TEST_PORT = 5000;
-
 describe('/games', () => {
-    let userId, server;
-    const makeRequest = (method, path, callback) => {
-        let headers = {};
-        if (userId) {
-            headers = { 'Cookie' : `userId=${userId}` };
-        }
-        http.request({
-            method: method,
-            port: TEST_PORT,
-            path: path,
-            headers: headers
-        }, callback).end();
-    };
-    
-    before(done => {
-        server = http.createServer(app).listen(TEST_PORT, done);
-    });
+    let agent, userId;
     
     beforeEach(done => {
-        makeRequest('GET', '/', (response) => {
-            var userIdRegex = /userId=([^;]*)/;
-            userId = userIdRegex
-                .exec(response.headers['set-cookie'][0])[1];
-            done();
-        });
-    });
-    
-    afterEach(() => {
-        const gamesCreated = gamesService.availableTo("non-user");
-        gamesCreated.forEach(game => game.remove());
-        userId = null;
-    });
-    
-    after(done => {
-        server.close(done);
+        agent = request.agent(app);
+        agent
+            .get('/')
+            .end((error, response) => {
+                var userIdRegex = /userId=([^;]*)/;
+                userId = userIdRegex
+                  .exec(response.headers['set-cookie'])[1];
+                done();
+            });
     });
     
     describe('/:id DELETE', () => {
         it('should allow users to delete their own games', done => {
-            const game = gamesService.create(userId, 'test');
+            gamesService.create(userId, 'test');
+            let game = gamesService.createdBy(userId)[0];
             
-            makeRequest('DELETE', '/games/' + game.id, response => {
-                expect(response.statusCode).to.equal(200);
-                expect(gamesService.createdBy(userId)).to.be.empty;
-                done();
-            });
+            agent
+                .delete('/games/' + game.id)
+                .expect(200)
+                .expect(() =>
+                  expect(gamesService.createdBy(userId)).to.be.empty) 
+                .end(done);
         });
     });
 });
